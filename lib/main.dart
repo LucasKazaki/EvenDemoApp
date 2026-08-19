2b99cc30227629161b3a80e58bd8fa22cdf6e108
LucasKazaki <93287580+LucasKazaki@users.noreply.github.com>
2026-08-19 03:12:54 -0400

    Initialize secure mobile services

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,31 @@
-
 import 'package:demo_ai_even/ble_manager.dart';
 import 'package:demo_ai_even/controllers/evenai_model_controller.dart';
 import 'package:demo_ai_even/views/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   BleManager.get();
   Get.put(EvenaiModelController());
-  
-  runApp(MyApp());
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Even AI Demo',
+      title: 'Agent Studio G1',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color(0xFFF4F5F7),
       ),
-      home: HomePage(), 
+      home: const HomePage(),
     );
   }
 }
